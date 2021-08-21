c102603b71bcb5138c80ef5670e9a0a393746b56
Vamsi Kilaru <vamsi.techdeveloper@gmail.com>
2021-08-21 11:32:42 -0700

    commit

--- datamodel_postgres/etl.py
+++ datamodel_postgres/etl.py
@@ -1,5 +1,6 @@
 import os
 import glob
+from pandas.core.indexes.base import Index
 import psycopg2
 import pandas as pd
 from sql_queries  import *
@@ -17,4 +18,64 @@ def process_song_file(cur,filepath):
         cur.execute(song_table_insert,song_data)
     print(f"Records inserted for file : {filepath}")
 
-        
+def process_log_file(cur,filepath):
+    # open log
+    df = pd.read_json(filepath,lines=True)  
+    # filter by next song action     
+    df = df[df['page']=="NextSong"].astype({'ts':'datetime64[ms]'})
+    #convert timestamp to datetime
+    t = pd.Series(df['ts'],index=df.index)
+    #insert time data records
+    column_lables = ["timestamp","hour","day","weekofyear","month","year","weekday"]
+    time_data =[]
+    for data in t:
+        time_data.append([data,data.hour,data.day,data.weekofyear,data.month,data.year,data.day_name()])
+    time_df = pd.DataFrame.from_records(data=time_data,columns=column_lables)
+
+    for i,row in time_df.iterrows():
+        cur.execute(time_table_insert,list(row))
+    # user table
+    user_df = df[['userId','firstName','lastName','gender','level']]
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert,row)
+    # insert song play records
+    for index,row in df.iterrows():
+        #get song play record from song_id and artist_id 
+        cur.execute(song_select,(row.song,row.artist,row.length))
+        result = cur.fetchone()
+        if result:
+            songid,artistid = result
+        else:
+            songid,artistid = None, None
+        #prepare song data
+        songplay_data = (row.ts, row.userId, row.level, songid,artistid,row.sessionId,row.location,row.userAgent)
+        cur.execute(songplay_table_insert,songplay_data)
+
+def process_data(cur,conn,filepath,func):
+
+    all_files = []
+    for root,dirs,files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+    # total number of files
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files,filepath))
+    #iterate over files and process
+    for i,datafile in enumerate(all_files,1):
+        func(cur,datafile)
+        conn.commit()
+        print('{}/{} files processed'.format(i,num_files))
+
+def main():
+    conn = psycopg2.connect("host=localhost dbname=sparkifydb user=postgres password=password")
+    cur =conn.cursor()
+    process_data(cur,conn,filepath='data/song_data',func=process_song_file)
+    process_data(cur,conn,filepath='data/log_data',func=process_log_file)
+    cur.close()
+    conn.close()
+
+if __name__=="__main__":
+    main()
+    print("\n\n DATA PROCESS COMPLETED!! \n\n")
--- datamodel_postgres/sql_queries.py
+++ datamodel_postgres/sql_queries.py
@@ -59,9 +59,9 @@ songplay_table_insert = ("""INSERT INTO songplays
                         VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s) """)
 
 #update user level when conflict
-user_table_insert = ("""INSERT INTO users (userId, firstname, lastname, gender, level)
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level)
                     VALUES (%s, %s, %s, %s, %s)
-                    ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDE.LEVEL
+                    ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level
 """)
 
 song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year,duration)
